297d8c472efafe5ad669af6461ccfff1a1f1f98f
Sandra <sandrabalog@rocketmail.com>
2023-06-11 14:09:13 +0200

    working on blog detail page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebase';
+import { BlogSection } from '../components/BlogSection';
 
 export const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(db, 'blogs'),
+      (snapshot) => {
+        let list = [];
+        snapshot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setBlogs(list);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  console.log('blogs', blogs);
   return (
-    <h2>Home</h2>
-  )
-}
+    <div className="container-fluid pb-4 pt-4 padding">
+      <div className="container padding">
+        <div className="row mx-0">
+          <h2>Trending</h2>
+          <div className="col-md-8">
+            <BlogSection blogs={blogs} />
+          </div>
+          <div className="col-md-3">
+            <h2>Tags</h2>
+            <h2>Most Popular</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
