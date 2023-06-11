24928a627466733cec6ba0e0e1f91919dcec18e7
Sandra <sandrabalog@rocketmail.com>
2023-06-11 12:03:24 +0200

    finish upload img funcionality

--- src/pages/AddEditBlog.js
+++ src/pages/AddEditBlog.js
@@ -1,7 +1,169 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import ReactTagInput from '@pathofdev/react-tag-input';
+import '@pathofdev/react-tag-input/build/index.css';
+import { storage } from '../firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
+const initialState = {
+  title: '',
+  tags: [],
+  trending: 'no',
+  category: '',
+  description: '',
+};
+
+const categoryOption = [
+  'Fashion',
+  'Technology',
+  'Food',
+  'Business',
+  'Politics',
+  'Sports',
+];
 export const AddEditBlog = () => {
+  const [form, setForm] = useState(initialState);
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(null);
+
+  const { title, tags, trending, category, description } = form;
+
+  useEffect(() => {
+    const uploadFile = () => {
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          setProgress(progress);
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setForm((prev) => ({ ...prev, imgUrl: downloadURL }));
+          });
+        }
+      );
+    };
+    file && uploadFile();
+  }, [file]);
+
+  const handleChange = (e) => {};
+  const handleTags = (tags) => {};
+  const handleTrending = (e) => {};
+  const onCategoryChange = (e) => {};
+
   return (
-    <div>AddEditBlog</div>
-  )
-}
+    <div className="container-fluid mb-4">
+      <div className="container">
+        <div className="col-12">
+          <div className="text-center heading py-2">Create Blog</div>
+        </div>
+        <div className="row h-100 justify-content-center align-items-center">
+          <div className="col-10 col-md-8 col-lg-6">
+            <form className="row blog-form">
+              <div className="col-12 py-3">
+                <input
+                  className="form-control input-text-box"
+                  type="text"
+                  placeholder="Title"
+                  name="title"
+                  value={title}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-12 py-3">
+                <ReactTagInput
+                  tags={tags}
+                  placeholder="Tags"
+                  onChange={handleTags}
+                />
+              </div>
+              <div className="col-12 py-3">
+                <p className="trending">Is it trending blog ?</p>
+                <div className="form-check-inline mx-2">
+                  <input
+                    type="radio"
+                    className="form-check-input"
+                    name="radioOption"
+                    value="yes"
+                    onChange={handleTrending}
+                    checked={trending === 'yes'}
+                  />
+                  <label htmlFor="radioOption" className="form-check-label">
+                    Yes&nbsp;
+                  </label>
+                  <input
+                    type="radio"
+                    className="form-check-input"
+                    name="radioOption"
+                    value="no"
+                    onChange={handleTrending}
+                    checked={trending === 'no'}
+                  />
+                  <label htmlFor="radioOption" className="form-check-label">
+                    No
+                  </label>
+                </div>
+              </div>
+              <div className="col-12 py-3">
+                <select
+                  value={category}
+                  onChange={onCategoryChange}
+                  className="catg-dropdown"
+                >
+                  <option>Please select category</option>
+                  {categoryOption.map((option, index) => (
+                    <option value={option || ''} key={index}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-12 py-3">
+                <textarea
+                  name="description"
+                  className="form-control description-box"
+                  placeholder="Description"
+                  value={description}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="file"
+                  className="form-control"
+                  onChange={(e) => setFile(e.target.files[0])}
+                />
+              </div>
+              <div className="col-12 py-3 text-center">
+                <button
+                  className="btn btn-add"
+                  type="submit"
+                  disabled={progress !== null && progress < 100}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
